3f45829822c188379762509a41b6cadd8688b559
Korki55 <37299197+Korki55@users.noreply.github.com>
2018-12-07 21:56:43 +0100

    Update Annotation.swift

--- MapboxARKit/Annotation.swift
+++ MapboxARKit/Annotation.swift
@@ -6,11 +6,13 @@ public class Annotation: NSObject {
     public var calloutImage: UIImage?
     public var anchor: MBARAnchor?
     public var tag: String!
+    public var fileName: String!
     
     public init(location: CLLocation, calloutImage: UIImage?, tag: String!, fileName: String) {
         self.location = location
         self.calloutImage = calloutImage
         self.tag = tag
+        self.fileName = fileName
     }
     
 }
